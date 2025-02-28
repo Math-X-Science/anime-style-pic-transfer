a768fe7bf683eb47834d6d7671d8b4aa99b4ba2b
MrXnneHang <XnneHang@gmail.com>
2025-02-28 22:55:03 +0800

    :art: typings.

--- src/converter/_typing.py
+++ src/converter/_typing.py
@@ -7,6 +7,6 @@ from typing import Literal
 # 模型选项
 ModelName = Literal["AnimeGANv3_Shinkai_37"]
 # 分辨率选项
-Resolution = Literal["240p", "720p"]
+Resolution = Literal["240p", "720p", "1080p"]
 # 超分辨率选项
 Scale = Literal["2x", "4x"]
