cbb87100510f410f12315485f7f614b41dcf0789
MrXnneHang <XnneHang@gmail.com>
2025-03-31 09:31:39 +0800

    :white_check_mark: test: Testing for _path_parser.

---
+++ src/converter/test.py
@@ -0,0 +1,24 @@
+from converter._path_parser import PathParser
+
+
+
+def main():
+    parser = PathParser()
+    print(parser.ffmpeg)
+    print(parser.ffprobe)
+    print(parser.model_path)
+    print(parser.realesr_excu)
+    print(parser.realesr_model)
+    print(parser.workspace)
+    print(parser.input)
+    print(parser.output)
+    print(parser.tmp)
+    print(parser.input_image)
+    print(parser.output_image)
+    print(parser.upscale_image)
+    print(parser.input_video)
+    print(parser.output_video)
+
+
+if __name__ == "__main__":
+    main()
